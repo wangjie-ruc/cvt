96a767511a7cf22f7a939bfe2a7a41f5c07633d0
jie.wang <jie.wang@vistel.cn>
2019-11-23 22:22:55 +0800

    config

---
+++ cvt/config.py
@@ -0,0 +1,19 @@
+
+
+
+class Config:
+
+    def from_dict(self):
+        pass
+
+    def from_json(self):
+        pass
+
+    def from_file(self):
+        pass
+
+    def from_option(self):
+        pass
+
+    def from_yaml(self):
+        pass
